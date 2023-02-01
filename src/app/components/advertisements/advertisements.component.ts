1ed8d69f72ae623ecbebcd20307945ca43fbf5a9
Victor Koitie <vickoweb@gmail.com>
2023-02-01 17:14:54 +0300

    Advertisment Update

--- src/app/components/advertisements/advertisements.component.ts
+++ src/app/components/advertisements/advertisements.component.ts
@@ -16,10 +16,8 @@ export class AdvertisementsComponent implements OnInit {
   public accessToken:string = ''  
   public user_id:number = 0
 
-  public BusinessRegistration: FormGroup | any;
   public assemblies:any = []
   private unsubscribe: Subscription[] = [];
-  private msg:string = ''  
   public password:string =''
   public passwordValue:string =''
 
@@ -27,17 +25,15 @@ export class AdvertisementsComponent implements OnInit {
   public messageResponse:string = ''
 
   // parameters
-  public adsList:any = []
-  public pendingadsList:any = []
+  public ratingList:any = []
+  public ratingModalData:any = []
+  public verifiedRatingCount:number = 0
+  public pendingAdsList:any = []
   public pendingAdsCount:number = 0
-  public activeAdsCount:number = 0
-  public AdsCount:number = 0
-  public businessModalData:any = []
-
-  // business Def Parameters
-  public businessOwnerId:number = 0
-  public businessCategory:number = 0
-  public assemblyId:number = 0
+  public rejectedRatingsList:any = []
+  public rejectedRatingsCount:number = 0
+  public allRatingsCount:number = 0
+  public reviewCount:number = 0
 
   // validation parameters
   public validity:boolean = false
@@ -54,26 +50,19 @@ export class AdvertisementsComponent implements OnInit {
   ptOptions: any = {};
   ptTrigger: Subject<any> = new Subject<any>();
 
+  rtOptions: any = {};
+  rtTrigger: Subject<any> = new Subject<any>();
+
+  public approvedRating: any = 0
+  public pendingRating: any = 0
+
   constructor(
     private fb: FormBuilder,    
     private toaster: ToastService,
     private modalService: NgbModal,
     private cbfService: CbfService
-  ) {
-    this.createForm();
-  }
-  
-  createForm() {
-    this.BusinessRegistration = this.fb.group({
-      fullName: ['', [Validators.required ]],
-      email: ['', [Validators.required ,  Validators.email, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
-      password: ['', [Validators.required, Validators.minLength(6)] ],
-		  assembly: ['', [Validators.required] ],
-      phoneNumber: ['', [Validators.required] ],
-    })
-  }
-  
-
+  ) {  }
+   
   ngOnInit(): void {
 
     // datatable
@@ -89,6 +78,7 @@ export class AdvertisementsComponent implements OnInit {
         'pdf'
       ]
     };
+
     this.ptOptions = {
       pagingType: 'full_numbers',
       pageLength: 10,
@@ -102,28 +92,240 @@ export class AdvertisementsComponent implements OnInit {
       ]
     };
 
-    this.pendingAdsCount = 1
-    this.activeAdsCount = 1
-    this.AdsCount = 2
-    
+    this.rtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10,
+      processing: true,
+      buttons: [
+        'copy',
+        'print',
+        'csv',
+        'excel',
+        'pdf'
+      ]
+    };
+        
     this.accessToken =  this.cbfService.AccessToken
     this.user_id = Number(this.cbfService.currentUserValue)
    
+    this.getVerifiedAds()
+    this.getPendingRatings()
+    this.getRejectedRatings()
+
   }
   
   // modal mgt
-  openModal(content:any, size:string) {
+  openModal(content:any, size:string, type:string, data:any) {
     this.modalService.open(content, {
       centered: true,
       backdrop: 'static',
       size: size
     });
 
+    this.ratingModalData = data
+
+    if(type == 'verified'){
+      this.approvedRating = this.ratingModalData?.rating
+    } else if(type == 'pending'){
+      // pending
+      this.pendingRating = this.ratingModalData?.rating      
+    } else if(type == 'rejected'){      
+      this.approvedRating = this.ratingModalData?.rating
+    }
+
+
 	}
+
+  reviewModal(content:any, data:any) {
+    this.modalService.open(content, {
+      centered: true,
+      backdrop: 'static',
+    });
+
+    // data
+    this.ratingModalData = data
+
+	}
+
+  // -------------------------------------------------------------------- Endpoints Consumption
+ 
+  // Verified Ratings
+  getVerifiedAds() {
+    let status = true
+    const ratverSubscr = this.cbfService.getAdvertisements(status, this.accessToken)
+
+    .subscribe({
+      next: (response: any) => {
+        let queryResults = response
+        this.allRatingsCount = queryResults.count.all
+        this.verifiedRatingCount = queryResults.count.total
+        this.reviewCount = queryResults.count.reviews
+        this.ratingList = queryResults.results  
+
+        if(this.verifiedRatingCount > 0){
+          this.dtTrigger.next(this.ratingList)
+        }
+
+      },
+      error: (e:HttpErrorResponse) =>  console.log('Something went wrong, please try again')   
+    })
+    
+    this.unsubscribe.push(ratverSubscr);
+  }
+
+  // Pending Ratings
+  getPendingRatings() {
+    let status = false
+    const ratpenSubscr = this.cbfService.getAdvertisements(status, this.accessToken)
+
+    .subscribe({
+      next: (response: any) => {
+        let queryResults = response
+        this.pendingAdsCount = queryResults.count.total
+        this.pendingAdsList = queryResults.results 
+
+        if(this.pendingAdsCount > 0){
+          this.ptTrigger.next(this.pendingAdsList)
+        }
+        
+      },
+      error: (e:HttpErrorResponse) =>  console.log('Something went wrong, please try again')   
+    })
+    
+    this.unsubscribe.push(ratpenSubscr);
+  }
+
+  // Get Rejected
+  getRejectedRatings() {
+    const rejRatSubscr = this.cbfService.getRejectedBusinessRatings(this.accessToken)
+
+    .subscribe({
+      next: (response: any) => {
+        let queryResults = response
+        this.rejectedRatingsCount = queryResults.count
+        this.rejectedRatingsList = queryResults.results 
+
+        if(this.rejectedRatingsCount > 0){
+          this.rtTrigger.next(this.rejectedRatingsList)
+        }
+        
+      },
+      error: (e:HttpErrorResponse) =>  console.log('Something went wrong, please try again')   
+    })
+    
+    this.unsubscribe.push(rejRatSubscr);
+  }
+
+
+  deleteRating(data: any) {
+
+    let modalData = data
+    let ratingId = modalData.id
+
+    const delData:FormData = new FormData()
+    delData.append('is_deleted', true.toString())
+    delData.append('modified_by', this.user_id.toString())
+
+    const delSubscr = this.cbfService.updateBusinessRating(ratingId, delData, this.accessToken)
+
+    .subscribe({
+      next: (response: any) => {
+        
+        if(response.id){
+          this.messageResponse = 'Rating successfully deleted'
+
+          setTimeout(() => {
+            window.location.reload()
+          }, 1200);
+          
+        }
+        
+      },
+      error: (e:HttpErrorResponse) =>  {        
+        this.messageResponse = 'Something went wrong, please try again' 
+      }   
+    })
+    
+    this.unsubscribe.push(delSubscr);
+
+  }
+
+  verifyRating(data: any) {
+
+    let modalData = data
+    let ratingId = modalData.id
+
+    const ratingData:FormData = new FormData()
+    ratingData.append('is_verified', true.toString())
+    ratingData.append('verified_by', this.user_id.toString())
+
+    const verSubscr = this.cbfService.updateBusinessRating(ratingId, ratingData, this.accessToken)
+
+    .subscribe({
+      next: (response: any) => {
+        
+        if(response.id){
+          this.messageResponse = 'Rating successfully verified'
+
+          setTimeout(() => {
+            window.location.reload()
+          }, 1200);
+          
+        }
+        
+      },
+      error: (e:HttpErrorResponse) =>  {        
+        this.messageResponse = 'Something went wrong, please try again' 
+      }   
+    })
+    
+    this.unsubscribe.push(verSubscr);
+
+  }
+
+  rejectRating(data: any) {
+
+    let modalData = data
+    let ratingId = modalData.id
+
+    const ratingData:FormData = new FormData()
+    ratingData.append('review_status', 'rejected')
+    ratingData.append('reason', modalData.reason)
+    ratingData.append('verified_by', this.user_id.toString())
+
+    const rejSubscr = this.cbfService.updateBusinessRating(ratingId, ratingData, this.accessToken)
+
+    .subscribe({
+      next: (response: any) => {
+        
+        if(response.id){
+          this.messageResponse = 'Rating successfully rejected'
+
+          setTimeout(() => {
+            window.location.reload()
+          }, 1200);
+          
+        }
+        
+      },
+      error: (e:HttpErrorResponse) =>  {        
+        this.messageResponse = 'Something went wrong, please try again' 
+      }   
+    })
+    
+    this.unsubscribe.push(rejSubscr);
+
+  }
   
   featureComing() {
     this.messageResponse = 'Feature coming soon, please bare with us. Thank you.'
   }
-  
+
+  ngOnDestroy() {
+    this.dtTrigger.unsubscribe()
+    this.ptTrigger.unsubscribe()
+    this.rtTrigger.unsubscribe()
+  }
+
 }
 
