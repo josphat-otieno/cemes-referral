d9b82b43c325ec06d463e57163855028e229e248
Victor Koitie <vickoweb@gmail.com>
2023-03-03 09:58:02 +0300

    member-complaints component

---
+++ src/app/components/member-complaints/member-complaints.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member-complaints',
+  templateUrl: './member-complaints.component.html',
+  styleUrls: ['./member-complaints.component.css']
+})
+export class MemberComplaintsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
